93762e0fb1f7ad4fad6cfbc2bbbb5a78719865fc
Dabisa Kelava <dabisa.kelava@gmail.com>
2017-05-29 14:00:21 +0100

    fixed some warnings

--- src/rect/main.cpp
+++ src/rect/main.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 
-std::ostream& operator<< (std::ostream& stream, const Rectangle& rect);
+std::ostream& operator<<(std::ostream& stream, const Rectangle& rect);
 void printInput(int id, const Rectangle& rect);
 void printIntersection(const std::vector<unsigned>& ids, const Rectangle& rect);
 int process(const char* file_name);
@@ -19,70 +19,70 @@ int process(const char* file_name);
 
 int main(int argc, char* argv[]) {
 
-	if( argc >= 2 ) {
+    if( argc >= 2 ) {
 
-		return process(argv[1]);
+        return process(argv[1]);
 
-	} else {
+    } else {
 
-		cout << "Input file not specified." << endl;
-		return -1;
-	}
+        cout << "Input file not specified." << endl;
+        return -1;
+    }
 }
 
-std::ostream& operator<< (std::ostream& stream, const Rectangle& rect) {
-	stream << "(" << rect.getLeft() << "," << rect.getTop() << "), w=" << rect.getWidth() << ", h=" << rect.getHeight();
-	return stream;
+std::ostream& operator<<(std::ostream& stream, const Rectangle& rect) {
+    stream << "(" << rect.getLeft() << "," << rect.getTop() << "), w=" << rect.getWidth() << ", h=" << rect.getHeight();
+    return stream;
 }
 
-void printInput( int id, const Rectangle& rect ) {
-	cout << "      " << id + 1 << ": Rectangle at " << rect << "." << endl;
+void printInput(int id, const Rectangle& rect) {
+    cout << "      " << id + 1 << ": Rectangle at " << rect << "." << endl;
 }
 
-void printIntersection( const std::vector<unsigned>& ids, const Rectangle& rect ) {
+void printIntersection(const std::vector<unsigned>& ids, const Rectangle& rect) {
 
-	if( ids.size() >= 2 ) {
-		cout << "      Between rectangle ";
-		for(int i = 0; i < ids.size(); ++i) {
-			const char* separator = (i < ids.size() - 2) ? ", " : (i == ids.size() - 2) ? " and " : "";
-			cout << ids[i] + 1 << separator;
-		}
-		cout << " at " << rect << "." << endl;
-	}
+    if( ids.size() >= 2 ) {
+        cout << "      Between rectangle ";
+        for(unsigned i = 0; i < ids.size(); ++i) {
+            const char* separator = (i < ids.size() - 2) ? ", " : (i == ids.size() - 2) ? " and " : "";
+            cout << ids[i] + 1 << separator;
+        }
+        cout << " at " << rect << "." << endl;
+    }
 }
 
 int process(const char* file_name) {
 
-	try {
-		constexpr int MAX_RECTANGLES = 10;
+    try {
+        constexpr int MAX_RECTANGLES = 10;
 
-		vector<Rectangle> rectangles = JsonRectangleReader(file_name, MAX_RECTANGLES).read();
+        vector<Rectangle> rectangles = JsonRectangleReader(file_name, MAX_RECTANGLES).read();
 
-		cout << "Input:" << endl;
+        cout << "Input:" << endl;
 
-			for(int i = 0; i < rectangles.size(); ++i) {
-			printInput(i, rectangles[i]);
-		}
-		
-		cout << endl << "Intersections:" << endl;
+        for(unsigned i = 0; i < rectangles.size(); ++i) {
+            printInput(i, rectangles[i]);
+        }
 
-		IntersectionTree::generate(rectangles).visitInLevelOrder(
-			[] (const std::vector<unsigned>& ids, const Rectangle& rect) {
-				printIntersection(ids, rect);
-			});
+        cout << endl << "Intersections:" << endl;
 
-		cout << endl;
+        IntersectionTree::generate(rectangles).visitInLevelOrder(
+        [] (const std::vector<unsigned>& ids, const Rectangle& rect) {
+            printIntersection(ids, rect);
+        });
 
-		return 0;
+        cout << endl;
 
-	} catch(Json::RuntimeError err) {
+        return 0;
 
-		std::cout << "parsing error: " << err.what();
-		return -1;
+    } catch(Json::RuntimeError err) {
 
-	} catch(JsonRectangleReader::InvalidDataError err) {
+        std::cout << "parsing error: " << err.what();
+        return -1;
 
-		std::cout << "invalid data: " << err.what() << "\n";
-		return -1;
-	}
+    } catch(JsonRectangleReader::InvalidDataError err) {
+
+        std::cout << "invalid data: " << err.what() << "\n";
+        return -1;
+    }
 }
